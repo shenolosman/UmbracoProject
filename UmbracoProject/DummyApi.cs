5c27a6fe29495a24d464012f0c20820d89bbad91
Shenol Osman <shenolyalmazosman@gmail.com>
2023-01-05 21:24:16 +0100

    productpage

---
+++ UmbracoProject/DummyApi.cs
@@ -0,0 +1,60 @@
+﻿namespace UmbracoProject
+{
+    public class DummyApi
+    {
+        static string url= "https://dummyjson.com/products";
+        public static async Task<Root?> GetRootAsync()
+        {
+            var client = new HttpClient();
+            return await client.GetFromJsonAsync<Root?>(url);
+        }
+    }
+
+    public class Product
+    {
+        public int id { get; set; }
+        public string? title { get; set; }
+        public double? price { get; set; }
+        public double? discountPercentage { get; set; }
+        public string? description { get; set; }
+        public double? rating { get; set; }
+        public int? stock { get; set; }
+        public string? brand { get; set; }
+        public string? category { get; set; }
+        public string? thumbnail { get; set; }
+        public string[]? images { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Product>? products { get; set; }
+    }
+    //public class Image
+    //{
+    //    public string? url { get; set; }
+    //}
+    //public class Category
+    //{
+    //    public string? name { get; set; }
+    //}
+    //"products": [
+    //    {
+    //  "id": 1,
+    //"title": "iPhone 9",
+    //"description": "An apple mobile which is nothing like apple",
+    //"price": 549,
+    //"discountPercentage": 12.96,
+    //"rating": 4.69,
+    //"stock": 94,
+    //"brand": "Apple",
+    //"category": "smartphones",
+    //"thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    //"images": [
+    //  "https://i.dummyjson.com/data/products/1/1.jpg",
+    //  "https://i.dummyjson.com/data/products/1/2.jpg",
+    //  "https://i.dummyjson.com/data/products/1/3.jpg",
+    //  "https://i.dummyjson.com/data/products/1/4.jpg",
+    //  "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+    //}
+    //]
+}
